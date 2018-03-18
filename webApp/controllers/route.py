8e27f69104bc33ca9f419e1bf5a19ebaaf6ae2dd
lkurniawan <linggar@outlook.com>
2018-03-18 23:00:29 +0700

    add new models

--- webApp/controllers/route.py
+++ webApp/controllers/route.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
+
 from webApp import app
 from flask import render_template, jsonify
+from webApp.library.connection import *
+from webApp.library.config import *
 
 
 @app.route('/')
 def start():
     return render_template('printer/index.html')
+
+
+@app.route('/testing')
+def testing():
+    conn = mysqlconnection(HOST, USERNAME, PASSWORD, DATABASE)
+    jabatan = conn.select('jabatan', None, 'id_jabatan, nama_jabatan')
+    return jsonify({'Message': 'Testing', 'value': jabatan})
+
+
+@app.route('/testing2')
+def testing2():
+    from webApp.models.Printer import status
+    jab = status()
+    state = 'manager'
+    print jab.get_jabatan(state)
+    return jsonify({'Message': 'list manager', 'value': jab.get_jabatan(state)})
 
 
 @app.route('/tahu')
--- webApp/models/Printer.py
+++ webApp/models/Printer.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import flash
+from webApp.library.connection import *
+from webApp.library.config import *
 
 
 class Printer(object):
@@ -10,6 +12,13 @@ class Printer(object):
         else:
             flash(text + "!!!")
 
+
+class status(object):
+    def get_jabatan(self, value):
+        conn = mysqlconnection(HOST, USERNAME, PASSWORD, DATABASE)
+        whereValue = 'nama_jabatan = %s'
+        jabatan = conn.select('jabatan', whereValue, 'id_jabatan, nama_jabatan', jabatan='manager')
+        return jabatan
 
 
 # process handling to database or other data source and bussiness logic
