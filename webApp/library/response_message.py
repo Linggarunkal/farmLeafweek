1d838c5cebb08cb013a8d5a4821923c3e1536e5c
Linggarunkal <linggar@outlook.com>
2018-04-10 17:49:20 +0700

    add profile front end

---
+++ webApp/library/response_message.py
@@ -0,0 +1,16 @@
+from flask import jsonify
+
+
+class messages(object):
+    def __init__(self, code):
+        self.code = code
+    def resp(self, data):
+        if self.code == 200:
+            json_message = 'Request Valid'
+            return jsonify({'Code': self.code, 'Message': json_message, 'Data': data})
+        elif self.code == 400:
+            json_message = 'Client Not Valid Request'
+            return jsonify({'Code': self.code, 'Message': json_message, 'Data': data})
+        elif self.code == 500:
+            json_message = 'Server Error Response Request'
+            return jsonify({'Code': self.code, 'Message': json_message, 'Data': data})
